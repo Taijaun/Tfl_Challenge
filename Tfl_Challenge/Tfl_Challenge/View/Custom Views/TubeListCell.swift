0a11026005bc76222670b91b8d17dd3980cfb8ad
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-10-20 17:07:47 +0100

    Added function to change background colour of cell depending on tube line name

--- Tfl_Challenge/Tfl_Challenge/View/Custom Views/TubeListCell.swift
+++ Tfl_Challenge/Tfl_Challenge/View/Custom Views/TubeListCell.swift
@@ -13,14 +13,43 @@ struct TubeListCell: View {
     // Digimon project has example
     let tubeName: String
     let tubeStatus: String
+    @State var color: Color?
     
     var body: some View {
         HStack{
             Text(tubeName)
             Spacer()
             Text(tubeStatus)
+        }.background(color)
+            .onAppear(){
+                getTubeLineColour(tubeColour: tubeName)
+            }
+    }
+    
+    func getTubeLineColour(tubeColour: String){
+        switch tubeColour{
+        case "Bakerloo":
+            self.color = .yellow
+        case "Central":
+            self.color = .red
+        default:
+            self.color = .clear
         }
     }
+}
+
+enum tubeColour {
+    case bakerloo
+    case central
+    case circle
+    case district
+    case hCity
+    case jubilee
+    case metropolitan
+    case northern
+    case piccadilly
+    case victoria
+    case wCity
 }
 
 #Preview {
